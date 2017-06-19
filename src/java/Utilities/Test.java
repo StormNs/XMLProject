7e905c53aa670d1965fdd6c4526c16041aa8a249
SONPHSE61822\StormNs <sonphse61822@fpt.edu.vn>
2017-06-19 19:34:11 +0700

    up

---
+++ src/java/Utilities/Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+/**
+ *
+ * @author StormNs
+ */
+public class Test {
+    public static void main(String[] args) {
+        String danh = "Chó Danh";
+        if(danh.equals("Chó Danh")){
+            System.out.println("fak Chó Danh");
+        }else{
+            System.out.println("chó Cường");
+        }
+    }
+}
